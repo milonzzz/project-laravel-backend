ea33ce5f9fd36afdfb1ac71aaebe77dad6b77129
mohamad sayontu <skaylark088@gmail.com>
2024-11-08 00:26:02 +0800

    first commit

---
+++ resources/views/pages/auth/product/index.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts.app')
+
+@section('title', 'Products')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Products</h1>
+                <div class="section-header-button">
+                    <a href="{{ route('product.create') }}" class="btn btn-primary">Add New</a>
+                </div>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="{{ url('home') }}">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Products</a></div>
+                    <div class="breadcrumb-item">All Products</div>
+                </div>
+            </div>
+            <div class="section-body">
+                <div class="row">
+                    <div class="col-12">
+                        @include('layouts.alert')
+                    </div>
+                </div>
+
+                <div class="row mt-4">
+                    <div class="col-12">
+                        <div class="card">
+
+                            <div class="card-body">
+
+                                <div class="float-right">
+                                    <form method="GET" action="{{ route('product.index') }}">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" placeholder="Search" name="name">
+                                            <div class="input-group-append">
+                                                <button class="btn btn-primary"><i class="fas fa-search"></i></button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                <div class="clearfix mb-3"></div>
+
+                                <div class="table-responsive">
+                                    <table class="table-striped table">
+                                        <tr>
+
+                                            <th>Name</th>
+                                            <th>Category</th>
+                                            <th>Price</th>
+                                            <th>Photo</th>
+                                            <th>Created At</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        @foreach ($products as $product)
+                                            <tr>
+
+                                                <td>{{ $product->name }}
+                                                </td>
+                                                <td>
+                                                    {{ $product->category }}
+                                                </td>
+
+                                                {{-- {{ $product->price }} --}}
+                                                <td> {{ sprintf('Rp. %s', number_format($product->price)) }}
+                                                </td>
+
+                                                <td>
+                                                    @if ($product->image)
+                                                        <img src="{{ asset('storage/products/' . $product->image) }}"
+                                                            alt="" width="100px" class="img-thumbnail">
+                                                    @else
+                                                        <span class="badge badge-danger">No Image</span>
+                                                    @endif
+
+                                                </td>
+                                                <td>{{ $product->created_at }}</td>
+                                                <td>
+                                                    <div class="d-flex justify-content-center">
+                                                        @if (auth()->user()->roles != 'USER')
+                                                            <a href='{{ route('product.edit', $product->id) }}'
+                                                                class="btn btn-sm btn-info btn-icon">
+                                                                <i class="fas fa-edit"></i> Edit
+                                                            </a>
+                                                        @endif
+                                                        @if (auth()->user()->roles == 'STAF' || auth()->user()->roles == 'ADMIN')
+                                                            <form action="{{ route('product.destroy', $product->id) }}"
+                                                                method="POST" class="ml-2">
+                                                                @method('DELETE')
+                                                                @csrf
+                                                                <button
+                                                                    class="btn btn-sm btn-danger btn-icon konfirmasi-hapus">
+                                                                    <i class="fas fa-times"></i> Hapus
+                                                                </button>
+                                                            </form>
+                                                        @endif
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+
+
+                                    </table>
+                                </div>
+                                <div class="float-right">
+                                    {{ $products->withQueryString()->links() }}
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('js/page/features-posts.js') }}"></script>
+@endpush
